89cda19fbf655f366d37c21e5b0f3bddcf540adc
A. Rahal <120186121+AbdelRahmanRahal@users.noreply.github.com>
2025-10-17 02:45:13 +0300

    feat: created a generic and logout button

---
+++ client/src/components/Button.tsx
@@ -0,0 +1,31 @@
+import "./Button.css"
+
+interface ButtonProps {
+  label: string
+  color?: string
+  className: string
+  onClick: () => void
+}
+
+const Button = ({
+  label,
+  color = "#1d87d8",
+  className,
+  onClick,
+}: ButtonProps) => {
+  const buttonStyle = {
+    "--button-color": color,
+  } as React.CSSProperties
+
+  return (
+    <button
+      className={className ? `${className} btn` : "btn"}
+      style={buttonStyle}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  )
+}
+
+export default Button
