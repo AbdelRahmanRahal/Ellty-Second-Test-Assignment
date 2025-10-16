6e9aeafb7d4efc5db30eb9eeed03c3a177e30f36
A. Rahal <120186121+AbdelRahmanRahal@users.noreply.github.com>
2025-10-16 20:34:04 +0300

    feat: created Textbox component
    It does not allow for anything but numbers

---
+++ client/src/components/Textbox.tsx
@@ -0,0 +1,32 @@
+import "./Textbox.css"
+
+interface TextboxProps {
+  placeholder?: string
+}
+
+const Textbox = ({ placeholder }: TextboxProps) => {
+  return (
+    <input
+      type="number"
+      className="textbox"
+      placeholder={placeholder}
+      onKeyDown={(event) => {
+        // Allow: backspace, tab, delete, arrows, and numbers
+        if (
+          ![
+            "Backspace",
+            "Tab",
+            "ArrowLeft",
+            "ArrowRight",
+            "Delete",
+          ].includes(event.key) &&
+          !/[0-9]/.test(event.key)
+        ) {
+          event.preventDefault();
+        }
+      }}
+    />
+  )
+}
+
+export default Textbox
