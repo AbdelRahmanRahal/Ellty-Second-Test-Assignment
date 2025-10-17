1ca362c06784d828549740bf4f0d826872699fe2
A. Rahal <120186121+AbdelRahmanRahal@users.noreply.github.com>
2025-10-17 12:14:24 +0300

    feat: production ready (I think)

--- server.js
+++ server.js
@@ -4,13 +4,30 @@ import userRoutes from "./routes/userRoutes.js"
 
 const app = express()
 const PORT = process.env.PORT || 3000
+const allowedOrigins = process.env.CORS_ORIGIN
+  ? process.env.CORS_ORIGIN.split(",")
+  : []
 
 // Middleware
-app.use(cors()) // Enable Cross-Origin Resource Sharing
+const corsOptions = {
+  origin: (origin, callback) => {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true)
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg =
+        "The CORS policy for this site does not allow access from the specified Origin."
+      return callback(new Error(msg), false)
+    }
+    return callback(null, true)
+  },
+}
+
+app.use(cors(corsOptions))
+// app.use(cors()) // Enable Cross-Origin Resource Sharing (FOR DEV ONLY)
 app.use(express.json()) // To parse JSON bodies
 
 // Import routes
-import postsRoutes from "./routes/postsRoutes.js";
+import postsRoutes from "./routes/postsRoutes.js"
 
 // API Routes
 app.use("/api/posts", postsRoutes)
